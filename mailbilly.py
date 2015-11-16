98feefc42a8539b8e3a259b9b5c8a39fa31644c4
g.e. link <link@grace.cat>
2015-11-16 16:46:02 +0100

    importing os

--- mailbilly.py
+++ mailbilly.py
@@ -1,8 +1,17 @@
+import os
+
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 
+parser = argparse.ArgumentParser(description='blah')
+parser.add_argument('--furreal', dest='furreal', action='store_true')
+
+if args.furreal:
+  app.config.from_pyfile('config.py')
+else:
+  app.config.from_pyfile('test_config.py')
+
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
 @app.route('/')
@@ -10,12 +19,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='blah')
-    parser.add_argument('--furreal', dest='furreal', action='store_true')
-
-    if args.furreal:
-      app.config.from_pyfile('config.py')
-    else:
-      app.config.from_pyfile('test_config.py')
-
-    app.run(host=app.config['HOST'], port=app.config['PORT'])
+    app.run(host=app.config['HOST'], port=app.config['PORT'])he
