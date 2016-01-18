bed26844dbd0ffbaa4ee2ca0062eaeb0434c8d07
g.e. link <link@grace.cat>
2016-01-18 15:56:40 +0100

    sslify flask

--- mailbilly.py
+++ mailbilly.py
@@ -3,12 +3,14 @@ import argparse
 
 from flask import Flask, render_template, request, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sslify import SSLify
 
 parser = argparse.ArgumentParser(description='blah')
 parser.add_argument('--furreal', help="do it for reals",
                     action="store_true")
 
 app = Flask(__name__)
+sslify = SSLify(app)
 
 args = parser.parse_args()
 print args.furreal
@@ -31,14 +33,7 @@ def index_fr():
 def index_en():
   print request.url, request.url.find("https") >= 0
   print request.is_secure
-  if request.url.find("https") >= 0:
-    print 'boo'
-    return render_template("index_en.html")
-  else:
-    print 'ahh'
-    httpUrl = request.url.replace("http://", "https://")
-    print httpUrl
-    return redirect(httpUrl)
+  return render_template("index_en.html")
 
 @app.route('/supersecretlongurlwhywouldyougohere')
 def staging():
@@ -46,4 +41,4 @@ def staging():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host=app.config['HOST'], port=port, debug=True)
+    app.run(host=app.config['HOST'], port=port)
